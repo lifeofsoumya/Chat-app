5bfb0a9ed5bdfa39f28cae1979b1be446f8236d5
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-04-06 21:51:01 +0530

    frontend sorted

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.static(__dirname + '/public'))
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, ()=>{console.log(`Listening to port ${port}`)});
 
 app.get('/', (req, res)=>{
-    
+    res.sendFile(__dirname + '/index.html')
 })
