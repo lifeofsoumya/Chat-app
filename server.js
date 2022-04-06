6ecfa584b7fcd53cc33b93a8623430cd7f28e347
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-04-06 23:40:02 +0530

    http added, socket.io installed

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-const express = require('express')
+const express = require('express');
 
 const app = express()
+const http = require('http').createServer(app)
 
 app.use(express.static(__dirname + '/public'))
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, ()=>{console.log(`Listening to port ${port}`)});
+http.listen(port, ()=>{console.log(`Listening to port ${port}`)});
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/index.html')
+})
+
+// Socket io setup
+const io = require('socket.io')(http);
+
+io.on('connection', (socket)=>{
+    console.log('socket is on bruh!')
 })
