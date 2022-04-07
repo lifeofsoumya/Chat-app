e53a9fa8d417d810a8e47e106507ad5d177c7474
lifeofsoumya <mondalsoumya2001@gmail.com>
2022-04-07 19:57:02 +0530

    client sided js added

--- public/client.js
+++ public/client.js
@@ -1,1 +1,39 @@
 const socket = io()
+
+let namE;
+let textarea = document.querySelector('#textarea')
+let messageArea = document.querySelector('.message__area')
+
+do {
+    namE = prompt('Enter your Name: ')
+} while(!namE);
+
+textarea.addEventListener('keyup', (e)=>{
+    if(e.key=== 'Enter'){
+        sendMessage(e.target.value)
+    }
+})
+
+function sendMessage(message){
+    let msg = {
+        user: namE,
+        message: message 
+    }
+    
+    // Append
+    appendMessage(msg, 'outgoing')
+}
+
+function appendMessage(msg, type){
+    let mainDiv = document.createElement('div')
+    let className = type
+    mainDiv.classList.add(className, 'message')
+
+    let markup = `
+        <h4>${msg.user}</h4>
+        <p>${msg.message}</p>
+    `
+
+    mainDiv.innerHTML = markup
+    messageArea.appendChild(mainDiv);
+}
